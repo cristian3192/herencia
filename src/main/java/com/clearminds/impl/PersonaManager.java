f2e25cc4a4cc7c0e64b6c9001908855e9b31cf8f
Cristian <cris_t_15@hotmail.com>
2020-12-10 10:46:24 -0500

    Se crea los servicios que implementan a la interface

---
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -0,0 +1,13 @@
+package com.clearminds.impl;
+
+import com.clearminds.interfaces.ServicioPersona;
+
+public class PersonaManager {
+	
+	ServicioPersona serv = null;
+	
+	public PersonaManager() {
+	
+	}
+
+}
